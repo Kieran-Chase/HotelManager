f012d5019be06b5bf1f5ba2b7405815b1d1bd7a2
QiCheng Wang <2139317487@qq.com>
2025-05-21 23:39:54 +0800

    5.21修改项目

--- src/com/coder/hotel/dao/impl/MemberLevelDaoImpl.java
+++ src/com/coder/hotel/dao/impl/MemberLevelDaoImpl.java
@@ -2,7 +2,9 @@ package com.coder.hotel.dao.impl;
 
 import com.coder.hotel.dao.MemberLevelDao;
 import com.coder.hotel.entity.MemberLevel;
+import com.coder.hotel.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +15,20 @@ import java.util.List;
 public class MemberLevelDaoImpl extends BaseDao<MemberLevel> implements MemberLevelDao {
     @Override
     public List<MemberLevel> selectByExample(MemberLevel level) {
+        String sql="select id,level,low,high,discount from memberlevel where 1=1";
+        List<Object> args=new ArrayList<Object>();
+        if(StringUtil.isNotEmpty(level.getLevel())){
+            sql+=" and level like ?";
+            args.add(level.getLevel());
+        }
+        if(level.getHigh()!=0){
+            sql+=" and high<= ?";
+            args.add(level.getHigh());
+        }
+        if(level.getLow()!=0){
+            sql+=" and low>= ?";
+            args.add(level.getLow());
+        }
         return null;
     }
 }

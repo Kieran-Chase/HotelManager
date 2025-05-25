2154457ba8314a21501dc44b4123897c26f66517
QiCheng Wang <2139317487@qq.com>
2025-05-25 23:41:00 +0800

    5.25修改项目

---
+++ src/com/coder/hotel/ui/memberInfo/MemberInfoUi.java
@@ -0,0 +1,33 @@
+/*
+ * Created by JFormDesigner on Sun May 25 23:40:48 CST 2025
+ */
+
+package com.coder.hotel.ui.memberInfo;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author bulang
+ */
+public class MemberInfoUi extends JFrame {
+    public MemberInfoUi() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
